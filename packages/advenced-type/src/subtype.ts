b325fc09af01003093555fcf3931998a30efbedb
dlrandy <1208484996@qq.com>
2019-09-11 06:40:41 +0800

    :pencil: subtype docs

--- packages/advenced-type/src/subtype.ts
+++ packages/advenced-type/src/subtype.ts
@@ -11,3 +11,24 @@ type NewUser = {
 function deleteUser(User:{id?: number, name: string}) {
   delete User.id
 }
+
+let existingUser: ExistingUser = {
+  id: 123456,
+  name: 'ima user'
+}
+deleteUser(existingUser);
+deleteUser(existingUser)
+existingUser.id
+
+
+type LegacyUser = {
+  id?: number | string,
+  name: string
+}
+
+let legacyUser:LegacyUser = {
+  id: '79331',
+  name: 'eric'
+}
+
+deleteUser(legacyUser);
